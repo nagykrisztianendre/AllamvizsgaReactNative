425c08157de89fe75e6e07fcff141a0ae8245341
Nagy Krisztian Endre <krisztian.nagy@3ss.tv>
2021-08-03 22:26:45 +0300

    Firebase Integration done

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {firebase} from "./enviroment/config";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+  <Button onPress={()=>storeHighScore(12,1)} title={"Press me!"}/>
     </View>
   );
 }
 
+function storeHighScore(userId, score) {
+  firebase
+      .database()
+      .ref('users/' + userId)
+      .set({
+        highscore: score,
+      });
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
