7d20803f4782f2ba2c2707f98522c4bdf768021f
Nagy Krisztian Endre <krisztian.nagy@3ss.tv>
2021-08-05 20:39:34 +0300

    Registration and Login UI created

--- App.js
+++ App.js
@@ -1,32 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import {firebase} from "./enviroment/config";
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import {Button, StyleSheet, Text, View} from 'react-native';
+// import {firebase} from "./enviroment/config";
+//
+//
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//   <Button onPress={()=>storeHighScore(12,1)} title={"Press me!"}/>
+//     </View>
+//   );
+// }
+//
+// function storeHighScore(userId, score) {
+//   firebase
+//       .database()
+//       .ref('users/' + userId)
+//       .set({
+//         highscore: score,
+//       });
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 
+import 'react-native-gesture-handler';
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {LoginScreen, HomeScreen, RegistrationScreen} from './src/screens';
+import {decode, encode} from 'base-64';
+
+if (!global.btoa) {
+    global.btoa = encode
+}
+if (!global.atob) {
+    global.atob = decode
+}
+
+const Stack = createStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-  <Button onPress={()=>storeHighScore(12,1)} title={"Press me!"}/>
-    </View>
-  );
-}
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
 
-function storeHighScore(userId, score) {
-  firebase
-      .database()
-      .ref('users/' + userId)
-      .set({
-        highscore: score,
-      });
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {user ? (
+                    <Stack.Screen name="Home">
+                        {props => <HomeScreen{...props} extraData={user}/>}
+
+                    </Stack.Screen>
+
+                ) : (
+                    <>
+                        <Stack.Screen name="Login" component={LoginScreen}/>
+                        <Stack.Screen name="Registration" component={RegistrationScreen}/>
+                    </>
+                )}
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
